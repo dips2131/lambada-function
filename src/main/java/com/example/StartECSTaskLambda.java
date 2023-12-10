1f8f1a8dee30eef169bb07daa3349b2391683860
Dipak Indrale <105654502+dips2131@users.noreply.github.com>
2023-12-10 15:38:19 +0530

    Create StartECSTaskLambda.java

---
+++ src/main/java/com/example/StartECSTaskLambda.java
@@ -0,0 +1,40 @@
+import com.amazonaws.services.ecs.AmazonECS;
+import com.amazonaws.services.ecs.AmazonECSClientBuilder;
+import com.amazonaws.services.ecs.model.RunTaskRequest;
+import com.amazonaws.services.ecs.model.RunTaskResponse;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StartECSTaskLambda implements RequestHandler<Object, String> {
+
+    private final String clusterName = "your-cluster-name";
+    private final String taskDefinitionArn = "your-task-definition-arn";
+
+    private final AmazonECS ecsClient = AmazonECSClientBuilder.defaultClient();
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        try {
+            // Specify the ECS task details
+            RunTaskRequest runTaskRequest = new RunTaskRequest()
+                    .withCluster(clusterName)
+                    .withTaskDefinition(taskDefinitionArn)
+                    .withCount(1); // Number of tasks to run
+
+            // Start the ECS task
+            RunTaskResponse runTaskResponse = ecsClient.runTask(runTaskRequest);
+
+            // Log task details (optional)
+            String taskArn = runTaskResponse.getTasks().get(0).getTaskArn();
+            System.out.println("ECS Task ID: " + taskArn);
+
+            return "ECS task started successfully!";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error starting ECS task";
+        }
+    }
+}
